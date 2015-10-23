5298b239e5d59a3b30951cda3023d8d7ba46a839
minicast <minicast@gmx.com>
2015-10-23 15:17:25 +0200

    fix(browser): add basic ui
    added the not yet component ui features

--- src/index.test.js
+++ src/index.test.js
@@ -6,6 +6,7 @@ import {expect} from 'chai';
 // import bolFormula from './index.js';
 const bolFormulaEditorUi = require('./index.js');
 
+
 describe("description", function () {
   it("description", function () {
     expect(true).to.be.true;
